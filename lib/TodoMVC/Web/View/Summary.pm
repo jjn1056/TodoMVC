7444f551c4f19bc9f0d32d42b3d8ba8d9ea9be2b
John Napiorkowski <jjn1056@yahoo.com>
2017-05-05 18:43:43 -0500

    neat up?

--- lib/TodoMVC/Web/View/Summary.pm
+++ lib/TodoMVC/Web/View/Summary.pm
@@ -8,39 +8,47 @@ with 'Template::Lace::ModelRole',
   'Catalyst::View::Template::Lace::Role::URI';
 
 has 'set' => (is=>'ro', required=>0);
-has 'tasks' => (is=>'ro', required=>1); 
-  
+has 'tasks' => (is=>'ro', required=>1);
+
+sub active_task_count { shift->tasks->active->count }
+ 
 sub process_dom {
   my ($self, $dom) = @_;
-  $dom->at('.todo-list li')
-    ->repeat(sub {
-        my ($dom, $item, $idx) = @_;
-        $dom->id('task'.$item->todo_id)
-          ->class($item->completed ? "completed": undef);
-        
-        $dom->at('label')
-          ->content($item->title)
-          ->attr('data-task', $item->todo_id);
-
-        $dom->at('input[name="title"]')->attr('value', $item->title);
-        $dom->at('input[name="completed"]')->attr('checked', 'on') if $item->completed;
-
-        $dom->at('.destroy')->attr('formaction', $self->uri('/task/delete', [$item->todo_id]));
-        $dom->at('form')
-          ->action($self->uri('/task/update', [$item->todo_id]))
-          ->id('form'.$item->todo_id);
-          
-      }, $self->tasks->all);
-
-  $dom->at('form#new_task')->action($self->uri('add'));
-  $dom->at('form#clear_completed')->action($self->uri('clear_completed'));
-
-  $dom->at('.todo-count strong')->content($self->tasks->active->count);
-
-  $dom->at('#active')->href( $self->uri('/summary',{q=>'active'}));
-  $dom->at('#completed')->href( $self->uri('/summary',{q=>'completed'}));
+  $self->todos($dom->at('.todo-list li'));
+  $dom->at('#new_task')->action($self->uri('add'));
+  $dom->at('#clear_completed')->action($self->uri('clear_completed'));
+  $dom->at('#active-count')->content($self->active_task_count);
+  $dom->at('#active')->href( $self->uri('summary',{q=>'active'}));
+  $dom->at('#completed')->href( $self->uri('summary',{q=>'completed'}));
   $dom->at("#${\$self->set}")->class('selected');
 }
+
+sub todos {
+  my ($self, $dom) = @_;
+  $dom->repeat(sub {
+    $self->todo(@_);
+  }, $self->tasks->all);
+}
+
+sub todo {
+  my ($self, $dom, $item) = @_;
+  $dom->id('task'.$item->todo_id)
+    ->class($item->completed ? "completed": undef);
+  
+  $dom->at('label')
+    ->content($item->title)
+    ->attr('data-task', $item->todo_id);
+
+  $dom->at('input[name="title"]')->attr('value', $item->title);
+  $dom->at('input[name="completed"]')->attr('checked', 'on') if $item->completed;
+  $dom->at('.destroy')->attr('formaction', $self->uri('/task/delete', [$item->todo_id]));
+
+  $dom->at('form')
+    ->action($self->uri('/task/update', [$item->todo_id]))
+    ->id('form'.$item->todo_id);
+}
+
+
 
 ## This is just a prototype for now.  Until we solve the 
 ## looping / context problem its just a toy.
